172a3d2b6326deb765eff3e390fb48f25207fbee
Yannick Schwartz <yannick.schwartz@cea.fr>
2011-08-23 11:40:44 +0200

    Moved the select.experiments method based on the search engine to array.search_experiments

--- pyxnat/core/array.py
+++ pyxnat/core/array.py
@@ -50,7 +50,7 @@ class ArrayData(object):
 
     def experiments(self, project_id=None, subject_id=None, subject_label=None,
               experiment_id=None, experiment_label=None,
-              experiment_type='xnat:imageSessionData', 
+              experiment_type='xnat:mrSessionData', 
               columns=None,
               constraints=None
               ):
@@ -87,8 +87,8 @@ class ArrayData(object):
 
     def scans(self, project_id=None, subject_id=None, subject_label=None,
               experiment_id=None, experiment_label=None,
-              experiment_type='xnat:imageSessionData', 
-              scan_type='xnat:imageScanData',
+              experiment_type='xnat:mrSessionData', 
+              scan_type='xnat:mrScanData',
               columns=None,
               constraints=None
               ):
@@ -125,3 +125,60 @@ class ArrayData(object):
                                experiment_id, experiment_label, 
                                experiment_type, constraints
                                )
+
+    def search_experiments(self, 
+                           project_id=None, 
+                           subject_id=None, 
+                           subject_label=None, 
+                           experiment_type='xnat:mrSessionData', 
+                           columns=None, 
+                           constraints=None
+                           ):
+
+        """ Returns a list of all visible experiment IDs of the 
+            specified type, filtered by optional constraints. This
+            function is a shortcut using the search engine.
+
+            Parameters
+            ----------
+            project_id: string
+                Name pattern to filter by project ID.
+            subject_id: string
+                Name pattern to filter by subject ID.
+            subject_label: string
+                Name pattern to filter by subject ID.
+            experiment_type: string
+                xsi path type must be a leaf session type. 
+                defaults to 'xnat:mrSessionData'
+            columns: List[string]
+                list of xsi paths for names of columns to return.
+            constraints: list[(tupple)]
+                List of tupples for comparison in the form (key, comparison, value)
+                valid comparisons are: =, <, <=,>,>=, LIKE
+            """
+
+        if columns is None:
+            columns = []
+
+        where_clause = []
+
+        if project_id is not None:
+            where_clause.append(('%s/project' % experiment_type, "=", project_id))
+        if subject_id is not None:
+            where_clause.append(('xnat:subjectData/ID', "=", subject_id))
+        if subject_label is not None:
+            where_clause.append(('xnat:subjectData/LABEL', "=", subject_label))
+
+        if constraints is not None:
+            where_clause.extend(constraints)
+
+        if where_clause != []:
+            where_clause.append('AND')
+
+        if where_clause != []:
+            table = self._intf.select(experiment_type, columns=columns).where(where_clause)
+            return table
+        else:
+            table = self._intf.select(experiment_type, columns=columns)
+            return table.all()
+
