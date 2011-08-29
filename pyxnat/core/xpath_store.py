b8871ff0e72fe9472b4e83103fc6bd3eefc3c095
Yannick Schwartz <yannick.schwartz@cea.fr>
2011-08-29 14:39:56 +0200

    Prototype of update method for xpath store

--- pyxnat/core/xpath_store.py
+++ pyxnat/core/xpath_store.py
@@ -1,4 +1,5 @@
-from glob import glob
+from os import path
+from glob import iglob
 
 from lxml import etree
 
@@ -7,27 +8,56 @@ class XpathStore(object):
 
     def __init__(self, interface):
         self._intf = interface
-        self._xml = None
+        self._tree = None
         self._nsmap = {}
 
     def __call__(self, xpath):
-        if self._xml is None:
+        if self._tree is None:
             self._load()
 
-        return self._xml.xpath(xpath, namespaces=self._nsmap)
+        return self._tree.xpath(xpath, namespaces=self._nsmap)
 
     def _load(self):
         roots = []
         nsmap = {}
 
-        for xml_file in glob('%s/*.xml' % self._intf._cachedir):
-            roots.append(etree.fromstring(open(xml_file, 'rb').read()))
+        for doc in iglob('%s/*.xml' % self._intf._cachedir):
+            roots.append(etree.fromstring(open(doc, 'rb').read()))
             nsmap.update(roots[-1].nsmap)
 
-        self._xml = etree.Element('Store')
-        self._xml.extend(roots)
+        self._tree = etree.Element('Store')
+        self._tree.extend(roots)
         self._nsmap = nsmap
 
-    def update(self, new=False):
-        pass
+    def _last_modified(self):
+        entry_point = self._intf._get_entry_point()
+        uri = '%s/subjects?columns=last_modified' % entry_point
+
+        return dict(JsonTable(self._intf._get_json(uri), 
+                              order_by=['ID', 'last_modified']
+                              ).select(['ID', 'last_modified']
+                                       ).items()
+                    )
+
+    def update(self, all_types=False):
+        """ Updates the xml files in the cachedir.
+
+            Parameters
+            ----------
+            all_type: boolean
+                If all_types is True, all the xml files will be updated
+                and downloaded if necessary. Checking of the local version
+                is up to date is only possible for subject XMLs.
+                If all_types is False, only the subject XMLs will be updated.
+        """
+
+        last_modified = self._last_modified()
+        
+        for doc in iglob('%s/*.xml' % self._intf._cachedir):
+            if is_subject(doc):
+                path.getmtime(doc)
+
+
+        # datetime.datetime.strptime(, '%Y-%m-%d %H:%M:%S')
+
 
