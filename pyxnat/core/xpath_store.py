867086b13a74f04a03638d228c1d463e14ffa55d
Yannick Schwartz <yannick.schwartz@cea.fr>
2011-08-30 13:37:11 +0200

    Added documentation and a few helpers to the xpath store.

--- pyxnat/core/xpath_store.py
+++ pyxnat/core/xpath_store.py
@@ -1,11 +1,32 @@
+import re
+from datetime import datetime
 from os import path
 from glob import iglob
 
 from lxml import etree
 
+from .jsonutil import JsonTable
+
+
+def get_subject_id(location):
+    f = open(location, 'rb')
+    content = f.read()
+    f.close()
+
+    subject = re.findall('<xnat:Subject\sID="(.*?)"\s', content)
+
+    if subject != []:
+        return subject[0]
+
 
 class XpathStore(object):
 
+    """ This utility class is used to query XML files describing XNAT 
+        subjects but which are stored in the cache directory. Xpath is
+        used as the query lanaguage but a few helpers are provided
+        to make simple queries.
+    """
+    
     def __init__(self, interface):
         self._intf = interface
         self._tree = None
@@ -21,9 +42,14 @@ class XpathStore(object):
         roots = []
         nsmap = {}
 
-        for doc in iglob('%s/*.xml' % self._intf._cachedir):
-            roots.append(etree.fromstring(open(doc, 'rb').read()))
-            nsmap.update(roots[-1].nsmap)
+        for location in iglob('%s/*.xml' % self._intf._cachedir):
+            f = open(location, 'rb')
+            content = f.read()
+            f.close()
+
+            if get_subject_id is not None:
+                roots.append(etree.fromstring(content))
+                nsmap.update(roots[-1].nsmap)
 
         self._tree = etree.Element('Store')
         self._tree.extend(roots)
@@ -39,25 +65,108 @@ class XpathStore(object):
                                        ).items()
                     )
 
-    def update(self, all_types=False):
+    def pull(self):
         """ Updates the xml files in the cachedir.
+        """
+        last_modified = self._last_modified()
+        
+        for location in iglob('%s/*.xml' % self._intf._cachedir):
+
+            subject_id = get_subject_id(location)
+
+            if subject_id is not None:
+                local_time = path.getmtime(location)
+                local_time = datetime.fromtimestamp(local_time)
+
+                server_time = last_modified[subject_id].split('.')[0]
+                server_time = datetime.strptime(server_time, 
+                                                '%Y-%m-%d %H:%M:%S')
+
+                if local_time < server_time:
+                    self._exec('%s/subjects/%s' % (
+                            self._intf._get_entry_point(), subject_id))
+
+    def clone(self, project_id):
+        """ Downloads all the subject XMLs for a project
 
             Parameters
             ----------
-            all_type: boolean
-                If all_types is True, all the xml files will be updated
-                and downloaded if necessary. Checking of the local version
-                is up to date is only possible for subject XMLs.
-                If all_types is False, only the subject XMLs will be updated.
+            project_id: str
+                The id of the project
         """
-
-        last_modified = self._last_modified()
+        for s in self._intf.select('/project/%s/subjects' % project_id):
+            s.get()
         
-        for doc in iglob('%s/*.xml' % self._intf._cachedir):
-            if is_subject(doc):
-                path.getmtime(doc)
+    def subjects(self):
+        """ Returns all the subject ids.
+        """
+        return self.__call__('//xnat:Subject/attribute::ID')
 
+    def keys(self):
+        """ Returns attribute keys at the subject level.
+        """
+        return self.element_keys('xnat:Subject')
 
-        # datetime.datetime.strptime(, '%Y-%m-%d %H:%M:%S')
+    def values(self, key):
+        """ Returns the values for all subjects for a specific attribute.
+        """
+        return self.element_values('xnat:Subject', key)
 
+    def attrs(self):
+        """ Returns the attributes of all subjects.
+        """
+        return self.element_attrs('xnat:Subject')
+
+    def elements(self):
+        """ Returns the element names for all subjects elements.
+
+            .. note:: in xpath terms, this function is returning all
+                the names of the subjects descendant nodes.
+        """
+        elements = set()
+
+        for element in self.__call__('//xnat:Subject/descendant::*'):
+            for ns in element.nsmap:
+                if element.nsmap[ns] in element.tag:
+                    n = element.tag.replace('{%s}' % element.nsmap[ns], 
+                                            '%s:' % ns)
+                    elements.add(n)
+
+        return list(elements)
+
+    def element_attrs(self, name):
+        """ Returns the attributes of this specific element.
+        """
+        attrs = []
+
+        for element in self.__call__('//%s' % name):
+            attrs.append(element.attrib)
+        
+        return attrs
+
+    def element_keys(self, name):
+        """ Returns the attribute keys of this specific element.
+        """
+        keys = set()
+
+        for element in self.__call__('//%s' % name):
+            for element_key in element.keys():
+                keys.add(element_key)
+        
+        return list(keys)
+
+    def element_values(self, name, key):
+        """ Returns the attribute values of this specific element.
+        """
+        values = set()
+
+        for subject in self.__call__('//%s' % name):
+            values.add(subject.get(key))
+        
+        return list(values)
+
+    def element_text(self, name):
+        """ Returns the text values of this specific element.
+        """
+        return list(set(self.__call__('//%s/child::text()' % name)))
 
