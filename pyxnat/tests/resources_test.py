45db9af23d2f23ceba330596ffec10c65969a347
jhuguetn <jhuguetn@gmail.com>
2019-02-25 13:00:41 +0100

    Replace '\n' end-line chars by an OS-agnostic alternative

--- pyxnat/tests/resources_test.py
+++ pyxnat/tests/resources_test.py
@@ -146,7 +146,7 @@ def test_get_file():
 
     fpath = fh.get()
     assert os.path.exists(fpath)
-    assert open(fpath, 'rb').read() == 'Hello XNAT!\n'
+    assert open(fpath, 'rb').read() == 'Hello XNAT!%s' % os.linesep
 
     custom = os.path.join(tempfile.gettempdir(), uuid1().hex)
     
@@ -167,7 +167,7 @@ def test_get_dir_file():
 
     fpath = fh.get()
     assert os.path.exists(fpath)
-    assert open(fpath, 'rb').read() == 'Hello again!\n'
+    assert open(fpath, 'rb').read() == 'Hello again!%s' % os.linesep
 
     custom = os.path.join(tempfile.gettempdir(), uuid1().hex)
     
@@ -181,7 +181,7 @@ def test_get_copy_file():
     fpath = subj_1.resource('test').file('hello.txt').get_copy(fpath)
     assert os.path.exists(fpath)
     fd = open(fpath, 'rb')
-    assert fd.read() == 'Hello XNAT!\n'
+    assert fd.read() == 'Hello XNAT!%s' % os.linesep
     fd.close()
     os.remove(fpath)
 
