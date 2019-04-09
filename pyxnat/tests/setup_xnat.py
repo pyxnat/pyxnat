c221aa1657e957a77f0df6f8c6940a0101c4f76f
xgrg <goperto@gmail.com>
2019-04-09 14:43:05 +0200

    FIX: cosmetics

--- pyxnat/tests/setup_xnat.py
+++ pyxnat/tests/setup_xnat.py
@@ -1,13 +1,11 @@
 import pyxnat
-import time
 x = pyxnat.Interface(config='.xnat.cfg')
 p = x.select.project('nosetests')
 p.create()
-for i in range(3,10):
+for i in range(3, 10):
     p = x.select.project('nosetests%s'%i)
     p.create()
 
 uri = '/data/projects/nosetests'
 options = {'alias': 'nosetests2'}
 data = x.put(uri, params=options).text
-print(x.select.project('nosetests').aliases())
