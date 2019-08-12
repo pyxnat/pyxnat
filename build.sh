985e9f0342943b3ab323d4dca32c10f7a8a107b4
xgrg <goperto@gmail.com>
2019-08-12 18:31:16 +0200

    DOC: script to build doc and push it to gh-pages

---
+++ build.sh
@@ -0,0 +1,20 @@
+python setup.py build_sphinx
+export COMMITTER_EMAIL="$(git log -1 --pretty="%cE")"
+export AUTHOR_NAME="$(git log -1 --pretty="%aN")"
+export INITIAL_COMMIT="$(git rev-list HEAD | tail -n 1)"
+echo $COMMITTER_EMAIL
+echo $AUTHOR_NAME
+
+rm -rf /tmp/html
+mv build/sphinx/html /tmp
+git config --local user.name $AUTHOR_NAME
+git config --local user.email $COMMITTER_EMAIL
+git checkout gh-pages
+git reset --hard $INITIAL_COMMIT # back to the initial commit
+git push origin gh-pages --force
+rm -r *
+mv /tmp/html/*  .
+touch .nojekyll
+git add --all
+git commit -m 'Update documentation'
+git push origin gh-pages
