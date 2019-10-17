d88e1ae6377998cbfaf10d75999a12d1f5404739
xgrg <goperto@gmail.com>
2019-10-17 18:19:35 +0200

    FIX: added missing decorators

--- pyxnat/tests/xpath_test.py
+++ pyxnat/tests/xpath_test.py
@@ -1,35 +1,30 @@
 import os.path as op
 from .. import Interface
-from . import PYXNAT_SKIP_NETWORK_TESTS
+from . import skip_if_no_network
 from nose import SkipTest
 
 central = Interface(config=op.join(op.dirname(op.abspath(__file__)), 'central.cfg'))
 
-def test_xpath_checkout():
-    if PYXNAT_SKIP_NETWORK_TESTS:
-        raise SkipTest('No network. Skipping test.')
+@skip_if_no_network
+def test_001_xpath_checkout():
     central.xpath.checkout(subjects=['OAS1_0001', 'OAS1_0002'])
     assert 'OAS1_0001' in central.xpath.subjects() and \
         'OAS1_0002' in central.xpath.subjects()
 
+@skip_if_no_network
 def test_elements():
-    if PYXNAT_SKIP_NETWORK_TESTS:
-        raise SkipTest('No network. Skipping test.')
     assert 'fs:region' in central.xpath.elements()
 
+@skip_if_no_network
 def test_keys():
-    if PYXNAT_SKIP_NETWORK_TESTS:
-        raise SkipTest('No network. Skipping test.')
     assert 'ID' in central.xpath.keys()
 
+@skip_if_no_network
 def test_values():
-    if PYXNAT_SKIP_NETWORK_TESTS:
-        raise SkipTest('No network. Skipping test.')
     assert 'OAS1_0002' in central.xpath.values('ID')
 
+@skip_if_no_network
 def test_element_attrs():
-    if PYXNAT_SKIP_NETWORK_TESTS:
-        raise SkipTest('No network. Skipping test.')
     assert isinstance(central.xpath.element_attrs('fs:region'), list)
 
     assert set(['SegId', 'hemisphere', 'name']).issubset(
