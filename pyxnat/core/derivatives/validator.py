a489a1861b3924f36d1a41ccc4baa43d88be1224
Greg Operto <goperto@gmail.com>
2019-12-11 17:48:37 +0100

    Add a wildcard in the validator name when collecting tests
    Idea: it is hard for end-user to provide full matching file name (which is composed validator name and MRSession label)
    
    Co-Authored-By: Jordi Huguet <j.huguet@amc.nl>

--- pyxnat/core/derivatives/validator.py
+++ pyxnat/core/derivatives/validator.py
@@ -2,7 +2,7 @@ XNAT_RESOURCE_NAMES = ['BBRC_VALIDATOR']
 
 def tests(self, name, key=None):
     import json
-    j = [e for e in list(self.files(name)) if 'json' in e._urn][0]
+    j = [e for e in list(self.files('{}*.json'.format(name)))][0]
     j = json.loads(self._intf.get(j._uri).text)
     if key is None:
         return j
