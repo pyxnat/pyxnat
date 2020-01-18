ef81312dcb072e6a815cd8e05385ffadab23399a
xgrg <goperto@gmail.com>
2020-01-18 20:22:26 +0100

    ENH: more derivatives

--- pyxnat/core/derivatives/cat12.py
+++ pyxnat/core/derivatives/cat12.py
@@ -18,3 +18,16 @@ def volumes(self):
             vols.append((kls, v))
 
         return dict(vols)
+
+def download_rc(self, path):
+    '''Downloads a local copy of DARTEL imports. (Keep in mind that in CAT12
+    images are named rp*, not rc*.'''
+    import os.path as op
+    rc_files = ['mri/rp1', 'mri/rp2']
+    for each in rc_files:
+        res = self._intf.array.experiments(experiment_id=self.parent().id(),
+            columns=['subject_label']).data[0]
+        subject_label = res['subject_label']
+        id = res['ID']
+        fp = '%s_%s_%s.nii.gz'%(each[-3:], subject_label, id)
+        list(self.files('%s*'%each))[0].get(op.join(path, fp))
