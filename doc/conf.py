5f3b9e332f977e345974d38b5cad4aea6d981872
baos1 <onealbao@gmail.com>
2020-06-14 22:11:56 -0500

    change versoin to 1.0.0.0.5 for pypi releasing purpose. fix sphinx extension (reference from https://github.com/pyxnat/pyxnat/blob/master/doc/conf.py) for bdist_wheel sdist building.

--- doc/conf.py
+++ doc/conf.py
@@ -29,13 +29,9 @@ sys.path.append(os.path.abspath('./sphinxext'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = [ 'sphinx.ext.autodoc', 
-               'sphinx.ext.pngmath', 
-               'numpydoc',
-               'phantom_import',
-               'autosummary',
-               'sphinx.ext.coverage'
-             ]
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.napoleon',
+              'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
