b9cd26b674e715b0f49a4b86888c1c42d11d1d25
xgrg <unknown>
2020-07-31 18:29:40 +0200

    PEP8 (bbrc)

--- pyxnat/core/derivatives/spm12.py
+++ pyxnat/core/derivatives/spm12.py
@@ -1,23 +1,25 @@
 XNAT_RESOURCE_NAME = 'SPM12_SEGMENT'
 
+
 def volumes(self):
-        import tempfile
-        import nibabel as nib
-        import numpy as np
+    import tempfile
+    import nibabel as nib
+    import numpy as np
 
-        vols = []
-        _, fp = tempfile.mkstemp(suffix='.nii.gz')
+    vols = []
+    _, fp = tempfile.mkstemp(suffix='.nii.gz')
 
-        names = ['c1', 'c2', 'c3']
-        for kls in names:
-            f = [each for each in self.files() if each.id().startswith(kls)][0]
-            f.get(fp)
-            d = nib.load(fp)
-            size = np.prod(d.header['pixdim'].tolist()[:4])
-            v = np.sum(d.dataobj) * size
-            vols.append((kls, v))
+    names = ['c1', 'c2', 'c3']
+    for kls in names:
+        f = [each for each in self.files() if each.id().startswith(kls)][0]
+        f.get(fp)
+        d = nib.load(fp)
+        size = np.prod(d.header['pixdim'].tolist()[:4])
+        v = np.sum(d.dataobj) * size
+        vols.append((kls, v))
 
-        return dict(vols)
+    return dict(vols)
+
 
 def download_rc(self, path):
     '''Downloads a local copy of DARTEL imports.'''
@@ -25,8 +27,8 @@ def download_rc(self, path):
     rc_files = ['rc1', 'rc2']
     for each in rc_files:
         res = self._intf.array.experiments(experiment_id=self.parent().id(),
-            columns=['subject_label']).data[0]
+                                           columns=['subject_label']).data[0]
         subject_label = res['subject_label']
         id = res['ID']
-        fp = '%s_%s_%s.nii.gz'%(each[-3:], subject_label, id)
-        list(self.files('%s*'%each))[0].get(op.join(path, fp))
+        fp = '%s_%s_%s.nii.gz' % (each[-3:], subject_label, id)
+        list(self.files('%s*' % each))[0].get(op.join(path, fp))
