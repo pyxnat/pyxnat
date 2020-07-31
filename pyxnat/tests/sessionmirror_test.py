deedcf5285ce117de72ce3e2e417e0e7136a6487
xgrg <>
2020-07-31 13:44:27 +0200

    FIX: changing destination projects from nosetests3 to testing_new
    For some reasons CENTRAL XNAT has started to give problems when
    mirroring a session in nosetests3, whereas everything seems fine in
    other projects.

--- pyxnat/tests/sessionmirror_test.py
+++ pyxnat/tests/sessionmirror_test.py
@@ -8,6 +8,9 @@ _modulepath = op.dirname(op.abspath(pyxnat.__file__))
 dd = op.join(op.split(_modulepath)[0], 'bin')
 sys.path.append(dd)
 
+dest_project = 'testing_new'
+
+
 @skip_if_no_network
 def test_001_sessionmirror():
     from sessionmirror import create_parser, main
@@ -15,11 +18,12 @@ def test_001_sessionmirror():
     cfg = op.join(op.dirname(op.abspath(__file__)), 'central.cfg')
     central = pyxnat.Interface(config=cfg)
     e = 'CENTRAL_E74609'
-    args = ['--h1', cfg, '--h2', cfg, '-e', e, '-p', 'nosetests3']
+    args = ['--h1', cfg, '--h2', cfg, '-e', e, '-p', dest_project]
     args = parser.parse_args(args)
     main(args)
-    e1 = central.array.experiments(experiment_id=e,
-                                  columns=['subject_label']).data[0]
+    central.array.experiments(experiment_id=e,
+                              columns=['subject_label']).data[0]
+
 
 @skip_if_no_network
 def test_002_deletesubject():
@@ -28,14 +32,16 @@ def test_002_deletesubject():
     central = pyxnat.Interface(config=cfg)
     e = 'CENTRAL_E74609'
     e0 = central.array.experiments(experiment_id=e,
-        columns=['subject_label', 'label']).data[0]
+                                   columns=['subject_label', 'label']).data[0]
     subject_label = e0['subject_label']
     experiment_label = e0['label']
-    e1 = central.array.experiments(project_id='nosetests3',
-                                subject_label=subject_label,
-                                experiment_label=experiment_label,
-                                columns=['subject_id']).data[0]
-    e2 = central.select.project('nosetests3').subject(e1['subject_ID']).experiment(e1['ID'])
+    e1 = central.array.experiments(project_id=dest_project,
+                                   subject_label=subject_label,
+                                   experiment_label=experiment_label,
+                                   columns=['subject_id']).data[0]
+    p = central.select.project(dest_project)
+    e2 = p.subject(e1['subject_ID']).experiment(e1['ID'])
     assert(e2.exists())
     e2.delete()
     assert(not e2.exists())
+
