8fed42a3dfc7e89af8abd7583f482b2b850c158f
xgrg <unknown>
2020-07-31 18:29:40 +0200

    PEP8 (bbrc)

--- pyxnat/tests/test_resource_functions.py
+++ pyxnat/tests/test_resource_functions.py
@@ -2,8 +2,8 @@ import os.path as op
 from pyxnat import Interface
 
 fp = op.join(op.dirname(op.abspath(__file__)), 'central.cfg')
-
 central = Interface(config=fp)
+
 
 def test_ashs_volumes():
     r = central.select.experiment('CENTRAL02_E01603').resource('ASHS')
