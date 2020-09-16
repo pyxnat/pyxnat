74b2f5aaaf55527be95af9b9bf46d62421950ee5
xgrg <goperto@gmail.com>
2020-09-16 17:39:56 +0200

    FIX: download_snapshot now supports jpg/png and creates multiple snapshots
    (whenever multiple snapshots are found in a report, it will store them
    in different files instead of building an ugly montage)

--- pyxnat/core/derivatives/validator.py
+++ pyxnat/core/derivatives/validator.py
@@ -29,76 +29,59 @@ def pdf(self, name):
 
 
 def download_snapshot(self, name, fp):
-    import os.path as op
+    """
+    Extract snapshot(s) from a validation report given its name.
+
+    Args:
+        name (String): Root found in the name of the report
+            (ex: FreeSurferValidator)
+        fp (String): Path to the file where the snapshot(s) will be saved.
+    """
+
     import os
+    import os.path as op
     import tempfile
-    import logging as log
-    from glob import glob
 
-    def extract_file(fp):
+    def extract_snapshots(fp):
+        import fitz
+        doc = fitz.open(fp)
         dn = op.dirname(fp)
-
-        with open(fp, "rb") as file:
-            pdf = file.read()
-
-        startmark = b"\xff\xd8"
-        startfix = 0
-        endmark = b"\xff\xd9"
-        endfix = 2
-        i = 0
-
-        njpg = 0
-        jpeg = []
-        while True:
-            istream = pdf.find(b"stream", i)
-            if istream < 0:
-                break
-            istart = pdf.find(startmark, istream, istream + 20)
-            if istart < 0:
-                i = istream + 20
-                continue
-            iend = pdf.find(b"endstream", istart)
-            if iend < 0:
-                raise Exception("Didn't find end of stream!")
-            iend = pdf.find(endmark, iend - 20)
-            if iend < 0:
-                raise Exception("Didn't find end of JPG!")
-
-            istart += startfix
-            iend += endfix
-            jpg = pdf[istart:iend]
-
-            fp = op.join(dn, "jpg%d.jpg" % njpg)
-            jpeg.append(fp)
-            with open(fp, "wb") as jpgfile:
-                jpgfile.write(jpg)
-
-            njpg += 1
-            i = iend
-        return jpeg
+        bn = op.splitext(op.basename(fp))[0]
+        images = []
+        xrefs = []
+        for page in range(len(doc)):
+            xrefs.extend([img[0] for img in doc.getPageImageList(page)])
+        xrefs = sorted(set(xrefs))
+        for k, xref in enumerate(xrefs[1:]):
+            pix = fitz.Pixmap(doc, xref)
+            fp_snap = op.join(dn, "{}_{}.png".format(bn, k))
+            if pix.n >= 5:    # CMYK: convert to RGB first
+                pix = fitz.Pixmap(fitz.csRGB, pix)
+            pix.writePNG(fp_snap)
+            images.append(fp_snap)
+        return images
 
     pdf = self.pdf(name)
 
     f, fp1 = tempfile.mkstemp(suffix='.pdf')
     os.close(f)
-    log.debug('Saving it in %s.' % fp1)
     pdf.get(dest=fp1)
 
-    jpeg = extract_file(fp1)
+    snaps = extract_snapshots(fp1)
 
-    if len(glob(op.join(op.dirname(fp1), '*.jpg'))) == 0:
+    if len(snaps) == 0:
         raise Exception('No snapshot found in report. %s %s' % (name, fp1))
 
-    f, fp2 = tempfile.mkstemp(suffix='.jpg')
-    os.close(f)  # we dont need the handle
-    cmd = 'montage %s/jpg*jpg -background black -geometry 2600x+0+0 '\
-        '-tile 1x %s' % (op.dirname(fp1), fp2)
-    os.system(cmd)
-
-    cmd = 'mv %s %s' % (fp2, fp)
-    os.system(cmd)
-    for each in jpeg:
-        cmd = 'rm %s' % each
+    files = []  # Will store the snapshots in their final folder
+    for i, each in enumerate(snaps):
+        bn, _ = op.splitext(fp)
+        _, ext = op.splitext(each)
+        interm = ''
+        if len(snaps) > 1:  # if multiple snapshots then add # in filename
+            interm = '_%s' % i
+        fp2 = '%s%s%s' % (bn, interm, ext)  # create destination filepath
+        cmd = 'mv %s %s' % (each, fp2)
         os.system(cmd)
-    cmd = 'rm %s' % fp1
-    os.system(cmd)
+        files.append(fp2)
+
+    return files
