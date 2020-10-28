a9c171185ad5b95366a6afba5a56cf112746f3ea
xgrg <goperto@gmail.com>
2020-10-28 15:03:17 +0100

    TESTS Added tests for resource-based functions

--- pyxnat/tests/test_resource_functions.py
+++ pyxnat/tests/test_resource_functions.py
@@ -3,10 +3,39 @@ from pyxnat import Interface
 
 fp = op.join(op.dirname(op.abspath(__file__)), 'central.cfg')
 central = Interface(config=fp)
+p = central.select.project('nosetests5')
+s = p.subject('rs')
+e1 = s.experiment('CENTRAL02_E01603')
+
+p = central.select.project('surfmask_smpl2')
+s = p.subject('CENTRAL05_S01120')
+e2 = s.experiment('CENTRAL05_E02681')
 
 
 def test_ashs_volumes():
-    r = central.select.experiment('CENTRAL02_E01603').resource('ASHS')
+
+    r = e1.resource('ASHS')
     hv = r.volumes()
     v = hv.query('region=="CA1" & side=="left"')['volume'].tolist()[0]
     assert(v == 1287.675)
+
+
+def test_freesurfer6_aparc():
+    r = e1.resource('FREESURFER6')
+    hv = r.aparc()
+    q = 'region=="supramarginal" & side=="left" & measurement=="CurvInd"'
+    v = hv.query(q)['value'].tolist()[0]
+    assert(v == '10.8')
+
+
+def test_freesurfer6_aseg():
+    r = e1.resource('FREESURFER6')
+    hv = r.aseg()
+    v = hv.query('region=="BrainSegVol"')['value'].tolist()[0]
+    assert(v == 2463095.0)
+
+
+def test_scandate():
+    s = e2.scans().first()
+    r = s.resource('DICOM')
+    assert(r.scandate() == '2008-05-06')
