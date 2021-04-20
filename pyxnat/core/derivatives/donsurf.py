d270a872fdf77bed7a0b5169989430c177444dd6
xgrg <goperto@gmail.com>
2021-04-20 10:37:14 +0200

    REF: less ambiguous import

--- pyxnat/core/derivatives/donsurf.py
+++ pyxnat/core/derivatives/donsurf.py
@@ -4,5 +4,5 @@ XNAT_RESOURCE_NAME = 'DONSURF'
 def aparc(self):
     """Returns cortical features as estimated by `DONSURF`."""
 
-    from .freesurfer import aparc
-    return aparc(self)
+    from .freesurfer import aparc as fs_aparc
+    return fs_aparc(self)
