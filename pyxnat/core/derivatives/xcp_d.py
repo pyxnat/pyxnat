fd8fca2b1d156c253730cedcab9065b4e0e332ff
jhuguetn <jhuguetn@gmail.com>
2023-07-18 11:57:41 +0200

    FEAT: xcp-d derivative

---
+++ pyxnat/core/derivatives/xcp_d.py
@@ -0,0 +1,25 @@
+XNAT_RESOURCE_NAMES = ['XCP_D']
+
+
+def conmat(self, atlas='DK'):
+    """Returns the functional connectivity matrix from the specified atlas."""
+    import pandas as pd
+    from io import StringIO
+
+    f = self.files(f'*_atlas-{atlas}_measure-pearsoncorrelation_conmat.tsv')[0]
+    content = self._intf.get(f._uri).content.decode('utf-8')
+    df = pd.read_table(StringIO(content), sep='\t').drop(columns=['Node'])
+
+    return df
+
+
+def timeseries(self, atlas='DK'):
+    """Returns the parcellated BOLD time-series from the specified atlas."""
+    import pandas as pd
+    from io import StringIO
+
+    f = self.files(f'*_atlas-{atlas}_timeseries.tsv')[0]
+    content = self._intf.get(f._uri).content.decode('utf-8')
+    df = pd.read_table(StringIO(content), sep='\t')
+
+    return df
