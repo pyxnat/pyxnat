FIX: avoid including pixdim[0]/qfac in voxel volume calculation

pyxnat/core/derivatives/cat12.py
@@ -16,7 +16,7 @@ def volumes(self):
         f = [each for each in self.files() if each.id().startswith(kls)][0]
         f.get(fp)
         d = nib.load(fp)
-        size = np.prod(d.header['pixdim'].tolist()[:4])
+        size = np.prod(d.header['pixdim'].tolist()[1:4])
         v = np.sum(d.dataobj) * size
         vols.append((kls, v))
 

pyxnat/core/derivatives/fsl_anat.py
@@ -41,7 +41,7 @@ def subcortical_volumes(self):
         f.get(fp)
         img = nib.load(fp)
         data = np.asarray(img.dataobj)
-        voxel_size = np.prod(img.header['pixdim'].tolist()[:4])
+        voxel_size = np.prod(img.header['pixdim'].tolist()[1:4])
 
         for roi, label in labels.items():
             volume = np.count_nonzero(data == label) * voxel_size